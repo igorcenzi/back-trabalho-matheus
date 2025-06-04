67f7781423cb0db00d086795f13a4e0df2683f67
igorcenzi <igor.cenzi2@gmail.com>
2025-06-04 11:23:40 -0300

    filtros

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from categories.models import Category
 from categories.serializers import CategorySerializer
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['is_active']
+    search_fields = ['name']
